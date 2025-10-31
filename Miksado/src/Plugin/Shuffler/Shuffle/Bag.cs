5ca940450bcc108636f47ca85fbd3d53c3d3bb3e
Jackson Parodi <jacksonaparodi@gmail.com>
2025-10-31 00:53:37 -0400

    hooray, fixed a bug in Bag shuffle live on stream

--- Miksado/src/Plugin/Shuffler/Shuffle/Bag.cs
+++ Miksado/src/Plugin/Shuffler/Shuffle/Bag.cs
@@ -1,6 +1,5 @@
 ﻿using Miksado.Plugin.Shuffler.Random;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Miksado.Plugin.Shuffler.Shuffle
 {
@@ -8,43 +7,42 @@ namespace Miksado.Plugin.Shuffler.Shuffle
     {
         // bag starts empty then fills up as games are shuffled into it
         // when full, empty the bag and start again
-        private readonly List<string> _bag = [];
+        private readonly List<string> _usedGameBag = [];
 
         public string NextGamePath(IRandomNumberGenerator rng, string[] allGamePaths, string? currentGamePath)
         {
             // as it is, bag algo can repeat a game if it is the only one left and then the first game of the new bag
             if (currentGamePath != null)
             {
-                _bag.Add(currentGamePath);
+                _usedGameBag.Add(currentGamePath);
             }
 
             // might be better to make sure each element of both arrays are the same
-            if (_bag.Count == allGamePaths.Length && _bag.All(allGamePaths.Contains))
+            if (_usedGameBag.Count >= allGamePaths.Length)
             {
-                _bag.Clear();
+                _usedGameBag.Clear();
             }
 
-            List<string> availableGames = [];
-
+            List<string> nextGameCandidates = [];
             foreach (string game in allGamePaths)
             {
-                if (!_bag.Contains(game))
+                if (!_usedGameBag.Contains(game))
                 {
-                    availableGames.Add(game);
+                    nextGameCandidates.Add(game);
                 }
             }
 
-            if (availableGames.Count == 0)
+            if (nextGameCandidates.Count == 0)
             {
                 string e = "zero available games";
                 e += " allGamePaths: " + string.Join(", ", allGamePaths);
-                e += " _bag: " + string.Join(", ", _bag);
+                e += " _bag: " + string.Join(", ", _usedGameBag);
                 e += " currentGamePath: " + currentGamePath;
                 e += " rng: " + rng.ToString();
                 throw new System.Exception(e);
             }
 
-            return availableGames[rng.Next() % availableGames.Count];
+            return nextGameCandidates[rng.Next() % nextGameCandidates.Count];
         }
     }
 }
